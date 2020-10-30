4493b91266f3783044df0da39e8ac866a94f8a75
tassk-work <tassk.work@gmail.com>
2020-10-30 23:30:53 +0900

    initial commit

--- config/sitemaps.py
+++ config/sitemaps.py
@@ -8,34 +8,49 @@ from blog.lib import common
 
 # codestart:BlogSitemap
 class BlogSitemap(Sitemap):
-    def get_language_codes(self, url):
+
+    def reverse(self, obj):
         pass
+
+    def location(self, obj):
+        path = self.reverse(obj)
+        if obj['language_code'] == settings.LANGUAGE_CODE:
+            return path
+        else:
+            return f'/{obj["language_code"]}{path}'
 
     def get_urls(self, page=1, site=None, protocol=None):
         urls = super().get_urls(page, site, protocol)
+
+        # alternate
+        kwalternates_w = {}
         for url in urls:
-            language_codes = self.get_language_codes(url)
-            if len(language_codes) < 2:
+            obj = url['item']
+            alternates = kwalternates_w.get(obj['key'], [])
+            kwalternates_w[obj['key']] = alternates
+            alternates.append({
+                'lang_code': obj['language_code'],
+                'location': url['location'],
+            })
+        # alternate x-default
+        kwalternates = {}
+        for key, alternates in kwalternates_w.items():
+            if len(alternates) < 2:
                 continue
-            alternates = []
-            default_location = url['location']
-            for language_code in language_codes:
-                if settings.LANGUAGE_CODE == language_code:
-                    location = url['location']
-                else:
-                    location = f'{protocol}://{site}/{language_code}{self.location(url["item"])}'
-                alternates.append({
-                    'lang_code': language_code,
-                    'location': location,
-                })
-                if language_code == 'en':
-                    default_location = location
-            
+            alternate_default = next((a for a in alternates if a['lang_code'] == 'en'), None)
+            if not alternate_default:
+                alternate_default = next((a for a in alternates if a['lang_code'] == settings.LANGUAGE_CODE), None)
             alternates.append({
                 'lang_code': 'x-default',
-                'location': default_location,
+                'location': alternate_default['location'],
             })
-            url['alternates'] = alternates
+            kwalternates[key] = alternates
+
+        for url in urls:
+            obj = url['item']
+            alternates = kwalternates.get(obj['key'])
+            if alternates:
+                url['alternates'] = alternates
         return urls
 # codeend:BlogSitemap
 
@@ -45,16 +60,16 @@ class AuthorSitemap(BlogSitemap):
     priority = 0.5
 
     def items(self):
-        return models.Author.objects.values('id','user__username').annotate(updated_date=Max('post__updated_date')).order_by('id')
+        # models.Author.objects.values('id','user__username').annotate(updated_date=Max('post__updated_date')).order_by('id')
+        items = models.PostContent.objects.values_list('post__author__user__username', 'language_code') \
+                .annotate(updated_date=Max('post__updated_date')).order_by('post__author_id')
+        return [{'key':item[0], 'language_code':item[1], 'updated_date':item[2]} for item in items]
 
-    def location(self, obj):
-        return reverse('blog:index', args=[obj['user__username']])
+    def reverse(self, obj):
+        return reverse('blog:index', args=[obj['key']])
 
     def lastmod(self, obj):
         return obj['updated_date']
-    
-    def get_language_codes(self, url):
-        return common.get_author_language_codes(url['item']['id'])
 # codeend:AuthorSitemap
 
 # codestart:PostSitemap
@@ -63,16 +78,21 @@ class PostSitemap(BlogSitemap):
     priority = 1.0
 
     def items(self):
-        return models.Post.objects.filter(status=models.PostStatus.PUBLIC).order_by('id')
+        items = models.PostContent.objects.values_list('post__id', 'post__author__user__username', 'language_code') \
+                .annotate(updated_date=Max('post__updated_date')).order_by('post__author_id')
+        return [{
+                'key': f'{item[0]},{item[1]}',
+                'id': item[0],
+                'auth_name': item[1],
+                'language_code': item[2],
+                'updated_date': item[3],
+            } for item in items]
 
-    def location(self, obj):
-        return reverse('blog:detail', args=[obj.author.user.username, obj.id])
+    def reverse(self, obj):
+        return reverse('blog:detail', args=[obj['auth_name'], obj['id']])
 
     def lastmod(self, obj):
-        return obj.updated_date
-    
-    def get_language_codes(self, url):
-        return common.get_post_language_codes(url['item'].id)
+        return obj['updated_date']
 # codeend:PostSitemap
 
 sitemaps = {
